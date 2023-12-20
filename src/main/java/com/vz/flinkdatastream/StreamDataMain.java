9b679ad54c541e91deaa59ae44e7f685a6b149e1
awsma <awsmanebadiger2023@gmail.com>
2023-12-19 21:44:51 -0500

    added init code

--- src/main/java/com/vz/flinkdatastream/StreamDataMain.java
+++ src/main/java/com/vz/flinkdatastream/StreamDataMain.java
@@ -2,13 +2,12 @@ package com.vz.flinkdatastream;
 
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StreamDataMain {
      static String TOPIC_IN = "adapt-inbound";
-     static String BOOTSTRAP_SERVER = "adapt-server:9092,adapt-server:9093,adapt-server:9094";
+     static String BOOTSTRAP_SERVER = "adapt-db-server:9092,adapt-db-server:9093,adapt-db-server:9094";
     static Logger logger = LoggerFactory.getLogger(StreamDataMain.class);
     public static void main(String[] args) {
         try {
